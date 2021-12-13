1b5c50e07f21c6bb1e82cf4a05ead4f5ce13734a
Marten Gartner <martenwallewein@web.de>
2021-12-13 18:21:16 +0100

    Save state

--- pathselection/pathselectionstore.go
+++ pathselection/pathselectionstore.go
@@ -1,6 +1,10 @@
 package pathselection
 
-import "github.com/scionproto/scion/go/lib/snet"
+import (
+	"sort"
+
+	"github.com/scionproto/scion/go/lib/snet"
+)
 
 type PeerPathEntry struct {
 	PeerAddrStr    string
@@ -17,16 +21,99 @@ type ConflictingPathResult struct {
 	NumPathsAvailable int
 }
 
-var PathSelectionStore map[string]PeerPathEntry
-
-func Init() {
-	PathSelectionStore = make(map[string]PeerPathEntry, 0)
+type PathSelectionStore struct {
+	data map[string]PeerPathEntry
 }
 
-// Calculates all potential conflicting (not disjoint) paths
-func GetConflictingPaths(entry PeerPathEntry) []ConflictingPathResult {
-	return []ConflictingPathResult{}
+func NewPathSelectionStore() *PathSelectionStore {
+	return &PathSelectionStore{
+		data: make(map[string]PeerPathEntry, 0),
+	}
 }
+
+// var PathSelectionStore map[string]PeerPathEntry
+
+// func Init() {
+//	PathSelectionStore = make(map[string]PeerPathEntry, 0)
+//}
+
+func pathsConflict(path1, path2 snet.Path) bool {
+	for _, intP1 := range path1.Metadata().Interfaces {
+		for _, intP2 := range path2.Metadata().Interfaces {
+			if intP1.IA.Equal(intP2.IA) && intP1.ID == intP2.ID {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Returns the pathIndex of the first conflicting path, or -1 if no conflicts
+func getPeerConflictPaths(path snet.Path, peer PeerPathEntry) int {
+	for i, targetPath := range peer.UsedPaths {
+		if pathsConflict(path, targetPath) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Sorts descending by the number of paths used
+func sortPeerPathEntries(entries []PeerPathEntry) []PeerPathEntry {
+	// TODO: Avoid in order sorting
+	sort.Slice(entries, func(i, j int) bool {
+		return len(entries[i].UsedPaths) < len(entries[j].UsedPaths)
+	})
+
+	return entries
+}
+
+func updatePeerEntryInStore(entry PeerPathEntry) {
+	PathSelectionStore[entry.PeerAddrStr] = entry
+}
+
+// TODO: Check the return value, maybe use pointer here...
+func removePathFromEntry(entry PeerPathEntry, pathIndex int) PeerPathEntry {
+	entry.UsedPaths = append(entry.UsedPaths[:pathIndex], entry.UsedPaths[pathIndex+1:]...)
+	return entry
+}
+
+// Used paths should be empty here...
+func AddPeerEntry(entry PeerPathEntry) {
+	potentialConflictingPeers := make([]PeerPathEntry, len(PathSelectionStore))
+	for _, v := range PathSelectionStore {
+		potentialConflictingPeers = append(potentialConflictingPeers, v)
+	}
+	potentialConflictingPeers = sortPeerPathEntries(potentialConflictingPeers)
+	for _, path := range entry.AvailablePaths {
+		for _, targetEntry := range potentialConflictingPeers {
+			conflictingPathIndex := getPeerConflictPaths(path, targetEntry)
+			if conflictingPathIndex >= 0 {
+				// Remove path from targetEntry
+				targetEntry = removePathFromEntry(entry, conflictingPathIndex)
+
+				// Replace targetEntry in map
+				updatePeerEntryInStore(targetEntry)
+
+				// Add to our entry
+				entry.UsedPaths = append(entry.UsedPaths, path)
+				break
+			}
+		}
+		// Update the list, so that we do not steal paths always from the first peer
+		potentialConflictingPeers = filterByMinimumUsedPaths(potentialConflictingPeers, len(entry.UsedPaths))
+
+	}
+
+}
+
+func filterByMinimumUsedPaths(entries []PeerPathEntry, minUsedPath int) []PeerPathEntry {
+	newEntries := make([]PeerPathEntry, len(PathSelectionStore))
+	return newEntries
+}
+
+// ---------------------------- Deprecated ---------------------------------------
 
 // How to use
 // We have a new peer with a set of (used and) available paths
@@ -41,6 +128,13 @@ func GetConflictingPaths(entry PeerPathEntry) []ConflictingPathResult {
 // Beginning Assumptions:
 // New peer has only 1 path
 // Current Peer has multiple paths
+
+// Calculates all potential conflicting (not disjoint) paths
+/*
+func GetConflictingPaths(entry PeerPathEntry) []ConflictingPathResult {
+
+	return []ConflictingPathResult{}
+}
 
 func sortConflicts([]ConflictingPathResult) {
 
@@ -67,3 +161,4 @@ func DoStuff(entry PeerPathEntry) {
 
 	}
 }
+*/
---
+++ pathselection/pathselectionstore_test.go
@@ -0,0 +1,13 @@
+package pathselection
+
+import "testing"
+
+// Test if adding a peer that does not conflict still uses all available paths
+func TestAddNonConflictingPeer(t *testing.T) {
+
+}
+
+// Test if adding the first peer to the store still uses all available paths
+func TestAddFirstPeer(t *testing.T) {
+
+}
